98c0822adebba85f81ab6558d4351e6be75e37e6
FeNjK <bodhisatva_xp@mail.ru>
2022-10-27 16:19:18 +0300

    add: добавлены доменные имена для фронтенда в cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  /* 'https://show-me.nomoredomains.club',
-  'http://show-me.nomoredomains.club', */
+  'https://find-and-save.nomoredomains.icu',
+  'http://find-and-save.nomoredomains.icu',
   'localhost:3000',
   'http://localhost:3000',
 ];
