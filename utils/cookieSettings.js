2d869b049d4ad97171dd18cbc93adfb63a96e6c8
FeNjK <bodhisatva_xp@mail.ru>
2022-12-03 14:59:37 +0300

    upgrade.

--- utils/cookieSettings.js
+++ utils/cookieSettings.js
@@ -1,7 +1,9 @@
 const cookieSettings = {
   maxAge: 3600000 * 24 * 7,
   httpOnly: true,
-  sameSite: true,
+  /* sameSite: true, */
+  sameSite: 'None',
+  secure: true,
 };
 
 module.exports = cookieSettings;
